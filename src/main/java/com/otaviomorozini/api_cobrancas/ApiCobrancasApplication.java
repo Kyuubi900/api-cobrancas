5a158cfd00c185fced334192c6c3cf62c0a0edcd
Otavio Morozini <otaviomorozini@hotmail.com>
2026-04-19 21:06:52 -0300

    feat: setup inicial do projeto com spring boot e configuracao do postgres

---
+++ src/main/java/com/otaviomorozini/api_cobrancas/ApiCobrancasApplication.java
@@ -0,0 +1,13 @@
+package com.otaviomorozini.api_cobrancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCobrancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCobrancasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otaviomorozini/api_cobrancas/ApiCobrancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otaviomorozini.api_cobrancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCobrancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
