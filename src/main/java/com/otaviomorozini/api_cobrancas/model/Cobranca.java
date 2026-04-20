cab5ad2f2a918ef9139a18152e0453dfa424ee03
Otavio Morozini <otaviomorozini@hotmail.com>
2026-04-19 21:38:20 -0300

    feat: cria entidades cliente e cobranca com mapeamento JPA

---
+++ src/main/java/com/otaviomorozini/api_cobrancas/model/Cobranca.java
@@ -0,0 +1,54 @@
+package com.otaviomorozini.api_cobrancas.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+public class Cobranca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal valor;
+    private LocalDate dataVencimento;
+    private String status;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
