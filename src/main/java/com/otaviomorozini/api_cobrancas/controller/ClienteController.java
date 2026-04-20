fa80874b4edbd77f7f4448192cbc46c85917a170
Otavio Morozini <otaviomorozini@hotmail.com>
2026-04-19 22:20:53 -0300

    feat: finaliza controllers com correcao de request body

---
+++ src/main/java/com/otaviomorozini/api_cobrancas/controller/ClienteController.java
@@ -0,0 +1,32 @@
+package com.otaviomorozini.api_cobrancas.controller;
+
+import com.otaviomorozini.api_cobrancas.model.Cliente;
+import com.otaviomorozini.api_cobrancas.service.ClienteService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+    @PostMapping
+    public ResponseEntity<Cliente> criarCliente(@RequestBody Cliente cliente){
+        return ResponseEntity.ok().body(clienteService.salvar(cliente));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Cliente>> listarTodosClientes(){
+        return ResponseEntity.ok().body(clienteService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> ListarClientePorId(@PathVariable Long id){
+        return ResponseEntity.of(clienteService.buscaPorId(id));
+    }
+}
---
+++ src/main/java/com/otaviomorozini/api_cobrancas/controller/CobrancaController.java
@@ -0,0 +1,29 @@
+package com.otaviomorozini.api_cobrancas.controller;
+
+import com.otaviomorozini.api_cobrancas.model.Cobranca;
+import com.otaviomorozini.api_cobrancas.service.CobrancaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cobrancas")
+public class CobrancaController {
+    private final CobrancaService cobrancaService;
+
+    public CobrancaController(CobrancaService cobrancaService) {
+        this.cobrancaService = cobrancaService;
+    }
+    @PostMapping
+    public ResponseEntity<Cobranca> gerarCobranca(@RequestBody Cobranca cobranca){
+        return ResponseEntity.ok().body(cobrancaService.gerar(cobranca));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Cobranca>> listarTodasCobrancas(){
+        return ResponseEntity.ok().body(cobrancaService.listaTodos());
+    }
+
+
+}
